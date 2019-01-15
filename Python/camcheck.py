d47f73d9e4c307b303c5febe872948a92f2178b9
BiggyBaron <dr.cleverest@gmail.com>
2019-01-15 14:40:19 +0600

    BugFix

--- Python/camcheck.py
+++ Python/camcheck.py
@@ -19,22 +19,3 @@ while(cap.isOpened()):  # check !
 # When everything is done release the capture
 cap.release()
 cv2.destroyAllWindows()
-
-
-
-def recognize_face():
-	ret, frame = cap.read()
-    number = 0
-	cv2.imshow('frame',frame)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-	try:
-		number = len(faces)
-		size = [faces[0][2], faces[0][3]]
-		if size[0] < 110:
-			number = 0
-	except:
-		pass
-	return number
